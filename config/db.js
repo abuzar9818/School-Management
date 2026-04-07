7c0df50ff670bdcade993bb3083ea9921fb76403
abuzar9818 <abuzarahmad678@gmail.com>
2026-04-08 01:34:57 +0530

    chore: migrate database configuration to environment variables

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
-const mysql=require("mysql2")
+const mysql=require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "yourpassword",
-    database: "schoolDB"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect(err => {
